dc271a732f257ec36a37fd2cd12b1ff643248a0b
subfare <71392621+subfare@users.noreply.github.com>
2026-06-16 11:48:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,17 +54,19 @@ function goLetters() {
 const letters = [
   `#1\n\nfør jeg møtte deg trodde jeg egentlig at jeg hadde null interesse i å slippe nye folk inn i mitt liv på den måten, jeg var ikke helt klar over hva jeg ville og jeg stolte ikke på nye folk som kunne påvirket meg på noe som helst måte`,
   `#2\n\ndu har vist meg at dette var feil måte å tenke på, du  har fått meg til å glede meg til de små tingene igjen som for eksempel bare kan være å sitte ved sidenav deg eller sitte å høre på deg snakke - du er den som har gjort hverdagen min lettere uten at du engang trenger å prøve alltid. du har klart å vise meg noe ingen andre har klart på en helt unik måte og det er trygghet`,
-  `Brev 3\n\nå være rundt deg føes ut som å være rundt noen som jeg merker at bryr seg om ting jeg har interesse, ting jeg gjør eller bare ting jeg sier, noen som prøver å være observant og noen som tar de tiltakene ingen andre har ville tatt for meg før deg
+  `#3\n\nå være rundt deg føes ut som å være rundt noen som jeg merker at bryr seg om ting jeg har interesse, ting jeg gjør eller bare ting jeg sier, noen som prøver å være observant og noen som tar de tiltakene ingen andre har ville tatt for meg før deg
 
 du er noen jeg har lyst å alltid kunne være rundt og med, og du er noen jeg har klart å gro meg nærmere med og noen jeg har lært meg å kunne like på absolutt alle ulike måter`,
-  `Brev 4\n\nNoe med deg føles riktig.`,
-  `Brev 5\n\nJeg tenker på deg ofte.`,
-  `Brev 6\n\nLitt for ofte.`,
-  `Brev 7\n\nMen jeg vil ikke stoppe.`,
-  `Brev 8\n\nDu gjør alt bedre.`,
-  `Brev 9\n\nSelv vanlige dager.`,
-  `Brev 10\n\nJeg forstår mer nå.`,
-  `Brev 11\n\nOg jeg vil ikke miste dette.`
+  `#4\n\nfor meg så er du ikke bare en 'noen', men du er en veldig spesiel noen - du er den noen som jeg vil tilbringe hele dagen min med og den noen som jeg tørr å spise rundt uten å  måtte tenke to ganger - du er også den noen som viser meg at det ikke er farlig å være den personen jeg har lyst å være og den personen jeg har lyst å vokse til å bli, og ha noen rundt meg som ikke har ett problem med det er noen jeg trenger i livet mitt.`,
+  `#5\n\nnår jeg tenger på framtide min så er du en av de første jeg ser for meg og jeg kommer aldri til å forvente at alt kommer til å være perfekt men heller fordi jeg vet at livet kommer til å bli mye lettere når jeg for dele det med deg`,
+  `#6\n\nden personen jeg er nå og har lust å bli trenger en som deg i sitt liv rajana, noen som viser at de kan være sårbar men samtidig noen som viser at de er mentalt strekt og det er det jeg ser i deg`,
+  `#7\n\ndu er noen som har vært igjennom mye og noen som bærer må masse man ikke skal bære på - selvom jeg ikke bærer på det samme så kan jeg føle deg, jeg kan forstå dine problemer og jeg kan være her for deg under de problemene `,
+  `#8\n\nvi, som to, skal jobbe sammen igjennom de problemene og hindringene som har oppstått og de som kommer til å oppstå, og det kommer til å bli hindringer jeg vet både du og meg kan komme oss igjennom hvis vi begge vil det.`,
+  `#9\n\nså takk for at du er den du er, det er nettop det som gjør deg til den rajana jeg har møtt og lært meg å elske. jeg elsker deg. ikke bare hvem du er men hvordan du puster på, hvordan du snakker og hvordan du ser på meg, hvordan øynene dine lyser opp når vi ser på hverandre og hvordan fregnene dine ser ut, de er perfekt plassert men du ser det ikke selv.`,
+  `#10\n\ndu er perfekt, og det er akkurat derfor jeg føler meg tiltrukket til deg fordi hadde jeg kunne endret noe i livet så hadde ingenting vært om hvem du er eller hvordan du ser ut, men heller vår status
+
+vi er ikke sammen, og det er noe jeg har vært redd for lenge - jeg kan ikke forklare hvorfor men jeg er bare redd. jeg er redd for alt og alle men du, du bare gir meg en trygghet`,
+  `#11\n\ndu er noen jeg stoler på rajana og du er noen som har gått fra å ikke bli invitert på tiktok danse-vidoen på lea sin bursdag til en av de viktigste personene som noen gang har vært involvert i mitt liv og jeg kan ikke love deg at jeg alltid kommer til å si de riktige tingene eller gjøre alt riktig men jeg kan love deg at  jeg alltid kommer til å velge deg, lytte til deg, støtte deg og alltid kjempe for oss, det er det  kjærlighet betyr for meg`
 ];
 
 function loadLetter(index) {
